b2275b0e2192519eb8059c271b563aec14b5d424
barbox11 <123384500+barbox11@users.noreply.github.com>
2026-05-20 11:15:28 -0500

    se configuro todo el frontend, se agregaron animaciones y se cambio el estilo de la pagina

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E10600",
+        dark: "#0B0B0B",
+        darkSoft: "#121212",
+      },
+      fontFamily: {
+        heading: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
-
